6af8f2e734493556ec2ca828f4fa0f880570fde9
HelenELee <HelenELee3@outlook.com>
2023-02-01 20:19:32 +0800

    added tests

---
+++ tests/Engineer.test.js
@@ -0,0 +1,71 @@
+const Employee = require("../lib/Engineer.js");
+
+describe("Engineer", () => {
+    describe("Initialization", () => {
+        it("should return an object containing a 'name', 'id' and 'email' properties when called with the 'new' keyword", () => {
+            const name = "John";
+            const id = 1;
+            const email = "John@mail.com";
+            const github = 
+
+            const obj = new Engineer(name, id, email);
+    
+            expect(obj.name).toBe("John");
+            expect(obj.id).toBe(1);
+            expect(obj.email).toBe("John@mail.com");
+        });      
+    });
+
+
+    describe("getName", () => {
+        it("should return the name property of the object", () => {
+            const name = "John";
+            const id = 1;
+            const email = "John@mail.com";
+
+            const obj = new Engineer(name, id, email);
+            expect(obj.getName()).toBe("John");
+            
+        });
+      });
+
+      describe("getId", () => {
+        it("should return the id property of the object", () => {
+            const name = "John";
+            const id = 1;
+            const email = "John@mail.com";
+
+            const obj = new Engineer(name, id, email);
+            expect(obj.getId()).toBe(1);
+            
+        });
+      });
+
+      describe("getEmail", () => {
+        it("should return the email property of the object", () => {
+            const name = "John";
+            const id = 1;
+            const email = "John@mail.com";
+
+            const obj = new Engineer(name, id, email);
+            expect(obj.getEmail()).toBe("John@mail.com");
+            
+        });
+      });
+
+      describe("getRole", () => {
+        it("should return the role property of the object", () => {
+            const name = "John";
+            const id = 1;
+            const email = "John@mail.com";
+
+            const obj = new Engineer(name, id, email);
+            expect(obj.getRole()).toBe("Employee");
+            
+        });
+      });
+
+
+
+}
+)
