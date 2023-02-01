d6f18f07ea8941579b00c64c55e6d017b66dad55
HelenELee <HelenELee3@outlook.com>
2023-02-01 20:14:52 +0800

    added validation

--- src/questions.js
+++ src/questions.js
@@ -1,3 +1,9 @@
+const validateEmail = require("./utilities.js");
+const githubUsernameRegex = require('github-username-regex');
+ 
+//const questionModule = require("./src/questions.js");
+var chalk = require('chalk');
+
 const managerQuestions = [
     {
         //first questions is instructions
@@ -6,22 +12,22 @@ const managerQuestions = [
         'Welcome to the Team Generator\n' +
         '--------------------------------\n' +
         'Use `npm run reset` to reset the dist/ folder.\n' +
-        'Are you ready to build yur team?',
+        'Are you ready to build your team?',
         name: 'welcome',
 
     },
     {
         type: 'input',
         message: "What is the team manager's name?",
-        name: 'manager',
+        name: 'name',
         //only ask next questions if user chose to continue after instructions were provided
         when: (answers) => answers.welcome === true, 
         //this question is mandatory
-        validate: proj => {
-          if (proj) {
+        validate: name => {
+          if (name) {
               return true;
           } else {
-              console.log("Please provide a manager's name!");
+              console.log(chalk.red("Please provide a manager's name!"));
               return false;
           }
         }
@@ -29,32 +35,34 @@ const managerQuestions = [
       {
         type: 'input',
         message: "What is the team manager's id?",
-        name: 'managerId',
+        name: 'id',
         //only ask next questions if user chose to continue after instructions were provided
         when: (answers) => answers.welcome === true, 
         //this question is mandatory
-        validate: proj => {
-          if (proj) {
+        validate: id => {
+            //console.log(typeof managerId);
+          if (id && /^[0-9]+$/.test(id)) {
               return true;
           } else {
-              console.log("Please provide a manager's id!");
-              return false;
+            console.log(chalk.red("\nPlease provide a valid manager's id. It must be numeric."));
+            return false;
           }
         }
       },
       {
         type: 'input',
         message: "What is the team manager's email?",
-        name: 'managerEmail',
+        name: 'email',
         //only ask next questions if user chose to continue after instructions were provided
         when: (answers) => answers.welcome === true, 
         //this question is mandatory
-        validate: proj => {
-          if (proj) {
+        validate: email => {
+          if (email && validateEmail(email)) {
             //TO DO - validation for email
               return true;
           } else {
-              console.log("Please provide a manager's email!");
+              console.log(chalk.red("\nPlease provide a valid manager's email!"));
+
               return false;
           }
         }
@@ -62,15 +70,16 @@ const managerQuestions = [
       {
         type: 'input',
         message: "What is the team manager's office number?",
-        name: 'managerNumber',
+        name: 'number',
         //only ask next questions if user chose to continue after instructions were provided
         when: (answers) => answers.welcome === true, 
         //this question is mandatory
-        validate: proj => {
-          if (proj) {
+        validate: number => {
+          //if (managerNumber && !isNaN(managerNumber)) {
+          if(number && /^[0-9]+$/.test(number)) {
               return true;
           } else {
-              console.log("Please provide a manager's office number!");
+              console.log(chalk.red("\nPlease provide a manager's office number. It must be numeric."));
               return false;
           }
         }
@@ -94,16 +103,16 @@ const managerQuestions = [
            // message: `What is your ${role}'s name?`,
             name: 'name',
             //only ask next questions if user chose to continue after instructions were provided
-            //when: (answers) => answers.welcome === true, 
+            when: (answers) => answers.role !=="None - I'm finished", 
             //this question is mandatory
-           /* validate: proj => {
-              if (proj) {
+           validate: name => {
+              if (name) {
                   return true;
               } else {
-                  console.log("Please provide a manager's name!");
+                  console.log(chalk.red(`\nPlease provide a name!`));
                   return false;
               }
-            }*/
+            }
           },
           {
             type: 'input',
@@ -111,16 +120,16 @@ const managerQuestions = [
             message: (answers) => `What is your ${answers.role}'s id?`,
             name: 'id',
             //only ask next questions if user chose to continue after instructions were provided
-            //when: (answers) => answers.welcome === true, 
+            when: (answers) => answers.role !=="None - I'm finished", 
             //this question is mandatory
-           /* validate: proj => {
-              if (proj) {
+            validate:id => {
+              if (id && /^[0-9]+$/.test(id)) {
                   return true;
               } else {
-                  console.log("Please provide a manager's name!");
+                  console.log(chalk.red(`\nPlease provide a valid id. It must be numeric.`));
                   return false;
               }
-            }*/
+            }
           },
           {
             type: 'input',
@@ -128,33 +137,69 @@ const managerQuestions = [
             message: (answers) => `What is your ${answers.role}'s email?`,
             name: 'email',
             //only ask next questions if user chose to continue after instructions were provided
-            //when: (answers) => answers.welcome === true, 
+            when: (answers) => answers.role !=="None - I'm finished", 
             //this question is mandatory
-           /* validate: proj => {
-              if (proj) {
+            validate: email => {
+              if (email && validateEmail(email)) {
                   return true;
               } else {
-                  console.log("Please provide a manager's name!");
+                  console.log(chalk.red(`\nPlease provide a valid email.`));
                   return false;
               }
-            }*/
+            }
           },
+          /*
           {
             type: 'input',
             //message: `What is your ${roleType}'s office number?`,
             message: (answers) => `What is your ${answers.role}'s office number?`,
             name: 'number',
             //only ask next questions if user chose to continue after instructions were provided
-            //when: (answers) => answers.welcome === true, 
+            when: (answers) => answers.role === "Manager",  
             //this question is mandatory
-           /* validate: proj => {
-              if (proj) {
+           validate: number => {
+              if (number & /^[0-9]+$/.test(number)) {
                   return true;
               } else {
-                  console.log("Please provide a manager's name!");
+                  console.log(chalk.red(`\nPlease provide a valid office number. It must be numeric.`));
                   return false;
               }
-            }*/
+            }
+          },
+          */
+          {
+            type: 'input',
+            //message: `What is your ${roleType}'s office number?`,
+            message: (answers) => `What is your ${answers.role}'s github username?`,
+            name: 'github',
+            //only ask next questions if user chose to continue after instructions were provided
+            when: (answers) => answers.role === "Engineer",  
+            //this question is mandatory
+           validate: github => {
+              if (githubUsernameRegex.test(github)) {
+                  return true;
+              } else {
+                  console.log(chalk.red(`\nPlease provide a valid github username.`));
+                  return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            //message: `What is your ${roleType}'s office number?`,
+            message: (answers) => `What is your ${answers.role}'s school?`,
+            name: 'school',
+            //only ask next questions if user chose to continue after instructions were provided
+            when: (answers) => answers.role === "Intern",  
+            //this question is mandatory
+           validate: school => {
+              if (school) {
+                  return true;
+              } else {
+                  console.log(chalk.red(`\nPlease provide a valid school.`));
+                  return false;
+              }
+            }
           },
     ]
 
